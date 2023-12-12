d514d7f414bf3f11073ae36e17136d08c613efbf
brgndy <brgndy96@gmail.com>
2023-12-12 11:00:11 +0900

    Feat: 페어매칭 참여자들 json형태로 관리

---
+++ src/database/participants.js
@@ -0,0 +1,181 @@
+import deepFreeze from '../utils/deepFreeze.js';
+
+const PARTICIPANTS = deepFreeze([
+  {
+    id: 1,
+    name: '백호',
+    course: '백엔드',
+  },
+  {
+    id: 2,
+    name: '태웅',
+    course: '백엔드',
+  },
+  {
+    id: 3,
+    name: '치수',
+    course: '백엔드',
+  },
+  {
+    id: 4,
+    name: '태섭',
+    course: '백엔드',
+  },
+  {
+    id: 5,
+    name: '대만',
+    course: '백엔드',
+  },
+  {
+    id: 6,
+    name: '준호',
+    course: '백엔드',
+  },
+  {
+    id: 7,
+    name: '대협',
+    course: '백엔드',
+  },
+  {
+    id: 8,
+    name: '덕규',
+    course: '백엔드',
+  },
+  {
+    id: 9,
+    name: '태산',
+    course: '백엔드',
+  },
+  {
+    id: 10,
+    name: '경태',
+    course: '백엔드',
+  },
+  {
+    id: 11,
+    name: '수겸',
+    course: '백엔드',
+  },
+  {
+    id: 12,
+    name: '현준',
+    course: '백엔드',
+  },
+  {
+    id: 13,
+    name: '준섭',
+    course: '백엔드',
+  },
+  {
+    id: 14,
+    name: '한나',
+    course: '백엔드',
+  },
+  {
+    id: 15,
+    name: '소연',
+    course: '백엔드',
+  },
+  {
+    id: 16,
+    name: '호열',
+    course: '백엔드',
+  },
+  {
+    id: 17,
+    name: '대남',
+    course: '백엔드',
+  },
+  {
+    id: 18,
+    name: '용팔',
+    course: '백엔드',
+  },
+  {
+    id: 19,
+    name: '구식',
+    course: '백엔드',
+  },
+  {
+    id: 20,
+    name: '달재',
+    course: '백엔드',
+  },
+  {
+    id: 21,
+    name: '보노',
+    course: '프론트엔드',
+  },
+  {
+    id: 22,
+    name: '시저',
+    course: '프론트엔드',
+  },
+  {
+    id: 23,
+    name: '쉐리',
+    course: '프론트엔드',
+  },
+  {
+    id: 24,
+    name: '신디',
+    course: '프론트엔드',
+  },
+  {
+    id: 25,
+    name: '다비',
+    course: '프론트엔드',
+  },
+  {
+    id: 26,
+    name: '덴버',
+    course: '프론트엔드',
+  },
+  {
+    id: 27,
+    name: '이브',
+    course: '프론트엔드',
+  },
+  {
+    id: 28,
+    name: '제시',
+    course: '프론트엔드',
+  },
+  {
+    id: 29,
+    name: '라라',
+    course: '프론트엔드',
+  },
+  {
+    id: 30,
+    name: '린다',
+    course: '프론트엔드',
+  },
+  {
+    id: 31,
+    name: '리사',
+    course: '프론트엔드',
+  },
+  {
+    id: 32,
+    name: '니콜',
+    course: '프론트엔드',
+  },
+  {
+    id: 33,
+    name: '로드',
+    course: '프론트엔드',
+  },
+  {
+    id: 34,
+    name: '윌터',
+    course: '프론트엔드',
+  },
+  {
+    id: 35,
+    name: '제키',
+    course: '프론트엔드',
+  },
+]);
+
+export default PARTICIPANTS;
