3d689f1dca6e930a3ff78b8d630b4342d319338e
brgndy <brgndy96@gmail.com>
2023-12-12 09:03:41 +0900

    Feat: 진행메세지 상수 설정

--- src/constants/messages/progressMessage.js
+++ src/constants/messages/progressMessage.js
@@ -2,6 +2,8 @@ import deepFreeze from '../../utils/deepFreeze.js';
 
 const PROGRESS_MESSAGE = deepFreeze({
   read_features: '기능을 선택하세요.\n1. 페어 매칭\n2. 페어 조회\n3. 페어 초기화\nQ. 종료\n',
+  select_course_level_mission:
+    '#############################################\n과정: 백엔드 | 프론트엔드\n미션:\n   - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n   - 레벨2: 장바구니 | 결제 | 지하철노선도\n   - 레벨3:\n   - 레벨4: 성능개선 | 배포   \n- 레벨5:\n############################################\n과정, 레벨, 미션을 선택하세요.\nex) 백엔드, 레벨1, 자동차경주\n',
 });
 
 export default PROGRESS_MESSAGE;
