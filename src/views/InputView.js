f28283fbec4445e3e71ccf22f296e6b1a2fadc00
brgndy <brgndy96@gmail.com>
2023-12-12 09:28:11 +0900

    Feat: README.md 에 있는 2번 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,11 +1,24 @@
 import { Console } from '@woowacourse/mission-utils';
 import PROGRESS_MESSAGE from '../constants/messages/progressMessage.js';
+import DELIMITER from '../constants/delimiters/delimiter.js';
 
 const InputView = {
   async readFeature() {
     const feature = await Console.readLineAsync(PROGRESS_MESSAGE.read_features);
 
     return feature;
+  },
+
+  async selectCourseAndLevelAndMission() {
+    const courseAndLevelAndMissionString = await Console.readLineAsync(
+      PROGRESS_MESSAGE.select_course_level_mission,
+    );
+
+    const [course, level, mission] = courseAndLevelAndMissionString
+      .split(DELIMITER.comma)
+      .map((info) => info.trim());
+
+    return { course, level, mission };
   },
 };
 
